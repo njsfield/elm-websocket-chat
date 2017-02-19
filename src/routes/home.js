cd52ae52cb36354d669aeddf4803cbba6f86eb15
Nick Field <heyimnick@live.com>
2017-02-19 13:00:05 +0000

    add injection point for websocket address

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,6 +3,8 @@ const path = require('path');
 
 module.exports = (request, response) => {
   fs.readFile(path.join(__dirname, '../../public/index.html'), (_, data) => {
+    data = data.toString();
+    data = data.replace(/{{socketport}}/, 'ws://localhost:8000');
     response.writeHead(200, {'Content-type': 'text/html'});
     response.write(data);
     response.end();
